6d02f6d3c816f98e7132469641f8de5fc46c3ec9
morgandstallion <morgansamuel960@gmail.com>
2026-06-17 16:32:00 +0000

    refactor(buttons): add className prop to PrimaryButton, SecondaryButton, GhostButton

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,24 +1,30 @@
 import { Link } from "react-router";
 
-const PrimaryButton = ({ title, path }) => {
+const PrimaryButton = ({ title, path, className = "" }) => {
   return (
-    <button className="bg-brand-60 hover:bg-brand-65 w-full lg:w-fit text-white font-normal py-[0.2em] px-[0.7em] rounded-md">
+    <button
+      className={`bg-brand-60 hover:bg-brand-65 text-white font-normal py-[0.2em] px-[0.7em] rounded-md ${className}`}
+    >
       <Link to={path}>{title}</Link>
     </button>
   );
 };
 
-const SecondaryButton = ({ title, path }) => {
+const SecondaryButton = ({ title, path, className = "" }) => {
   return (
-    <button className="bg-dark-10 border border-dark-15 hover:bg-dark-08 w-full lg:w-fit text-white font-normal py-[0.2em] px-[0.7em] rounded-md">
+    <button
+      className={`bg-dark-10 border border-dark-15 hover:bg-dark-08 text-white font-normal py-[0.2em] px-[0.7em] rounded-md ${className}`}
+    >
       <Link to={path}>{title}</Link>
     </button>
   );
 };
 
-const GhostButton = ({ title, path }) => {
+const GhostButton = ({ title, path, className = "" }) => {
   return (
-    <button className="py-[0.2em] px-[0.7em] w-full lg:w-fit rounded-md border border-dark-15">
+    <button
+      className={`py-[0.2em] px-[0.7em] rounded-md border border-dark-15 ${className}`}
+    >
       <Link to={path}>{title}</Link>
     </button>
   );
